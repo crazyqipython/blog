0bb82a579f59b09df942d13e1fe5d8d26748d5f0
umisk <3218237814@qq.com>
2016-10-04 21:24:25 +0800

    queryset

--- blog/models.py
+++ blog/models.py
@@ -2,9 +2,9 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-#class EntryQuerySet(models.QuerySet):
-    #def publish(self):
-        #return self.filter(publish = True)
+class EntryQuerySet(models.QuerySet):
+    def publish(self):
+        return self.filter(publish = True)
 
 class Entry(models.Model):
     """entry class"""
@@ -14,7 +14,7 @@ class Entry(models.Model):
     publish = models.DateTimeField(auto_now_add=True)
     created = models.DateTimeField(auto_now=True)
     #manage
-    #objects = EntryQuerySet.as_manager()
+    objects = EntryQuerySet.as_manager()
 
     def __str__(self):
         return self.title
