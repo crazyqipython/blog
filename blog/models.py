2efb9e3a4a09fe974445082fa00b5e00204a5344
umisk <3218237814@qq.com>
2016-10-05 11:35:58 +0800

    编码错误

--- blog/models.py
+++ blog/models.py
@@ -1,37 +1,41 @@
-from __future__ import unicode_literals
-
+#coding:utf-8
 from django.db import models
 from django.core.urlresolvers import reverse
 
+
 class Tag(models.Model):
-    slug = models.SlugField(max_length=200,unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+
     def __str__(self):
         return self.slug
 
-#????
+    class Meta:
+        verbose_name = "标签"
+        verbose_name_plural = "标签"
+
 class EntryQuerySet(models.QuerySet):
     def published(self):
-        return self.filter(publish = True)
+        return self.filter(publish=True)
+
 
 class Entry(models.Model):
-    """entry class"""
-    title = models.CharField(max_length = 200)
+    title = models.CharField(max_length=200)
     body = models.TextField()
-    slug = models.SlugField(max_length=200,unique = True)
+    slug = models.SlugField(max_length=200, unique=True)
     publish = models.BooleanField(default=True)
-    modified = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
-    tags = models.ManyToManyField(Tag)
-    #manage
+    modified = models.DateTimeField(auto_now=True)
+    tags = models.ManyToManyField(Tag,verbose_name='标签',)
+
     objects = EntryQuerySet.as_manager()
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse("entry_detail",kwargs = {"slug":self.slug})
+        return reverse("entry_detail", kwargs={"slug": self.slug})
 
     class Meta:
-        verbose_name = "Blog Entry"
-        verbose_name_plural = "Blog Entries"
+        verbose_name = "博客"
+        verbose_name_plural = "博客后台"
         ordering = ["-created"]
--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from . import models
 class BlogIndex(generic.ListView):
     queryset = models.Entry.objects.published()
     template_name = 'home.html'
-    paginate_by = 4
+    paginate_by = 2
 
 class BlogDetail(generic.DetailView):
     model = models.Entry
